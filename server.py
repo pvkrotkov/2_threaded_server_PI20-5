d29387a406bc662b20578de0bf8f72e7d62ea13a
Гагарин Ярослав <90040588+exerius@users.noreply.github.com>
2021-10-01 16:30:56 +0300

    Попытка сделать сервер многопоточным

--- server.py
+++ server.py
@@ -1,20 +1,26 @@
-import socket
+# -*- coding: utf-8 -*-
+import socket, threading
+
+
+def work_with_client(conn):
+    while True:
+        msg = ''
+        data = conn.recv(1024)
+        if not data:
+            break
+        msg += data.decode()
+        conn.send(data)
+    conn.close()
+
 
 sock = socket.socket()
-sock.bind(('', 9090))
+sock.bind(('127.0.0.1', 9091))
 sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+    try:
+        conn, addr = sock.accept()
+    except:
+        continue
+    else:
+        Thread(target=work_with_client, args=conn)
