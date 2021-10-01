94f19d658273f71a156b000484574ab53317e20f
Гагарин Ярослав <90040588+exerius@users.noreply.github.com>
2021-10-01 17:04:54 +0300

    Попытка сделать многопоточным

--- scanner.py
+++ scanner.py
@@ -1,15 +1,27 @@
+# -*- coding: utf-8 -*-
 import socket
 from threading import Thread
 
-N = 2**16 - 1
 
-for port in range(1,100):
-    sock = socket.socket()
-    try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
-        sock.close()
+
+
+
+def scan(first, last):
+    for port in range(first,last):
+        sock = socket.socket()
+        try:
+            print(port)
+            sock.connect(('127.0.0.1', port))
+            print("Порт", i, "открыт")
+        except:
+            continue
+        finally:
+            scanned.append(port)
+            sock.close()
+        
+N = 2**16 - 1
+unscanned = [i for i in range(1, N+1)]
+scanned = []
+
+for i in range(256):
+    Thread(target=scan, args=(i*int(N/257), (i+1)*int(N/257))).start()
