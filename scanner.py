de44ca2c23d010564b30f41f89a27e91c92bdb54
Гагарин Ярослав <gagarinyaroslav3@gmail.com>
2021-10-07 23:44:48 +0300

    Готово

--- scanner.py
+++ scanner.py
@@ -3,25 +3,35 @@ import socket
 from threading import Thread
 
 
-
-
-
 def scan(first, last):
     for port in range(first,last):
         sock = socket.socket()
         try:
-            print(port)
+            scanned.append(port)
             sock.connect(('127.0.0.1', port))
-            print("Порт", i, "открыт")
+            openned.append(port)
         except:
             continue
         finally:
-            scanned.append(port)
             sock.close()
-        
-N = 2**16 - 1
-unscanned = [i for i in range(1, N+1)]
-scanned = []
 
+
+def printing():
+    i = 0
+    while i < 65535:
+        if i in scanned:
+            i += 1
+            if i in openned:
+                print(f"Порт {i} открыт")
+            else:
+                print(f"Порт {i} закрыт")
+        else:
+            continue
+
+
+N = 2**16 - 1
+openned = []
+scanned = []
 for i in range(256):
     Thread(target=scan, args=(i*int(N/257), (i+1)*int(N/257))).start()
+Thread(target=printing).start()
