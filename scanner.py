b81cd0750db2f8907218b7368296e3bfe36fbacb
Гагарин Ярослав <90040588+exerius@users.noreply.github.com>
2021-10-08 15:55:56 +0300

    Неготово, пока пусть так

--- scanner.py
+++ scanner.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import socket
 from threading import Thread
-
+from progress.bar import IncrementalBar
 
 def scan(first, last): # сканируем порты
     for port in range(first,last): # по очереди
@@ -14,16 +14,15 @@ def scan(first, last): # сканируем порты
             continue
         finally:
             sock.close()
+            bar.next()
 
 
 def printing():
     i = 0
-    while i < 65535: # по очереди проходим все возмодные порты
-        if i in scanned: 
+    while i < N: # по очереди проходим все возмодные порты
+        if i in scanned:
             if i in openned: #если просканирован, пишем о его состоянии
                 print(f"Порт {i} открыт")
-            else:
-                print(f"Порт {i} закрыт")
             i += 1
         else: # если нет -- ждем сканирования
             continue
@@ -32,6 +31,7 @@ def printing():
 N = 2**16 - 1 # кол-во портов
 openned = [] # открытые порты
 scanned = [] #просканированые порты
+bar = IncrementalBar('Progress', max = N)
 for i in range(256): # создаем много потоков сканирования
-    Thread(target=scan, args=(i*int(N/257), (i+1)*int(N/257))).start() 
+    Thread(target=scan, args=(i*int(N/257), (i+1)*int(N/257))).start()
 Thread(target=printing).start() # создаем один поток вывода
