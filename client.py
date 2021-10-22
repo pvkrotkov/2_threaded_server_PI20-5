593e39eeb917d05e7d1e0b3c09eaf4288ea25e30
etemir <91737637+etemir@users.noreply.github.com>
2021-10-22 15:51:30 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,16 +1,41 @@
 import socket
-from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
+host = input("Введите адрес хоста или lh: ")
+if host == "lh":
+	host = 'localhost'
+else:
+	host_ls = host.split(".", 4)
+	for i in host_ls:
+		if 0 <= int(i) <= 255:
+			pass
+		else:
+			host = 'localhost'
+port = int(input("Введите адрес порта: "))
+if 1024 <= int(port) <= 65535:
+	pass
+
+sock.connect((host, port))
+print('Введите Ваше сообщение или "exit" для выхода: ')
+msg = ""
+while True:
+	cl_input = input()
+	if cl_input == "exit".strip():
+		a = 1
+		print("Выходим из программы")
+		break
+	msg += cl_input + " "
 sock.send(msg.encode())
 
-data = sock.recv(1024)
+print('Получаем ответ от сервера...')
 
-sock.close()
-
-print(data.decode())
+if (a == 1) and (msg == ''):
+	print('Disconnecting...')
+	sock.close()
+else:
+	data = sock.recv(1024)
+	print('Disconnecting...')
+	sock.close()
+	print("Вы ввели: ", data.decode()) 
