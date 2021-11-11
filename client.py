c8ad67c7bdb04a3a2b20a99f3f6896a8b166ab18
Jek4 <90418243+Jek4@users.noreply.github.com>
2021-11-11 15:14:44 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,14 @@
 import socket
-from time import sleep
-
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
+with socket.socket() as sock:
+    sock.connect(('localhost', 8083))
+    print('Соединение с сервером обнаружено')
+    while True:
+        line = input()
+        sock.send(line.encode("utf-8"))
+        print(f'Отправка серверу: {line}')
+        data = sock.recv(1024)
+        print(f"Получено от сервера: {data.decode('utf-8')}")
+        if line == "exit":
+            sock.close()
+            break
+print('Соединено с сервером разорвано')
