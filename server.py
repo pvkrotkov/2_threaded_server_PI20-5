9d94b64e27190aa57383b125c59ae652553652fe
Jek4 <90418243+Jek4@users.noreply.github.com>
2021-11-11 15:20:52 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,27 @@
 import socket
+import threading
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+class Thr(threading.Thread):
+    n = 0
+    def __init__(self, conn, addr):
+        threading.Thread.__init__(self, name="t" + str(Thr.n))
+        self.n = Thr.n
+        Thr.n=Thr.n+1
+        self.conn = conn
+        self.addr = addr
+        self.start()
+    def run(self):
+        while True:
+            data = self.conn.recv(1024)
+            if not data:
+                break
+            print("Процесс", self.n, "Получено: ", data.decode())
+            self.conn.send(data)
 
-msg = ''
-
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+with socket.socket() as sock:
+     threads = []
+     sock.bind(('', 8083))
+     sock.listen(0)
+     while True:
+         conn, addr = sock.accept()
+         threads.append(Thr(conn, addr))
