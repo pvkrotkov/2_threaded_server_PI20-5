688cd90564da9745d1f197c9109083ee0ac01b3c
Пупынин Савва <pupyninsavva@gmail.com>
2021-11-12 11:53:59 +0300

    Практика готова

--- scanner.py
+++ scanner.py
@@ -1,15 +1,83 @@
 import socket
-from threading import Thread
+import re
+from threading import Thread, Lock
+from queue import Queue
 
-N = 2**16 - 1
 
-for port in range(1,100):
+def getIP():
+    while True:
+        host = input('Введите ip который будем сканировать: ')
+        if re.match(r'^(\d{1,3}\.){3}\d{1,3}$', host):
+            return host
+        else:
+            print('Вы ввели некорректный адрес')
+
+
+
+def scanner(port, host):
+    global LOCK, open_ports
     sock = socket.socket()
     try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
+        sock.connect((host, port))
     except:
-        continue
+        pass
+    else:
+        with LOCK:
+            available.append(port)
     finally:
         sock.close()
+
+
+def scan_thread(host):
+    global portQueue
+    while True:
+        port = portQueue.get()
+        scanner(port, host)
+        portQueue.task_done()
+
+
+def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    if iteration == total: 
+        print()
+
+
+def prbar():
+    global all_ports, start, portQueue
+    while True:
+        all_cnt = all_ports-start
+        items = portQueue.qsize()
+        in_scan = all_cnt - items
+        printProgressBar(in_scan, all_cnt, ' Сканируется:', length = 50)
+        if not items:
+            break
+    
+
+def main(host, ports):
+    global portQueue, treads_count
+    for port_number in ports:
+        portQueue.put(port_number)
+    Thread(target=prbar, daemon = True).start()
+    for _ in range(treads_count):
+        Thread(target=scan_thread, args = [host], daemon = True).start()
+    portQueue.join()   
+
+
+
+host = getIP()
+all_ports = 2**16
+start = 1
+treads_count = 5000
+portQueue = Queue()
+LOCK = Lock()
+ports_list = [i for i in range(start,all_ports)]
+available = []
+
+
+
+main(host, ports_list)
+available.sort()
+print('Открытые порты:', '; '.join([str(i) for i in available]))
--- server.py
+++ server.py
@@ -1,20 +1,21 @@
-import socket
+import socket, threading
+
+def connect(conn, addr):
+	while True:
+		try:
+			data = conn.recv(1024)
+		except (ConnectionResetError, ConnectionAbortedError):
+			print(f'Клиент {addr} Разорвал соединение')
+			raise
+		print(f'Клиент передал сообщение {addr}: {data.decode()}')
+		conn.send(data)
+
 
 sock = socket.socket()
 sock.bind(('', 9090))
 sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+	conn, addr = sock.accept()
+	print(f'connected {addr}')
+	threading.Thread(target = connect, args = (conn, addr), daemon = True).start()
